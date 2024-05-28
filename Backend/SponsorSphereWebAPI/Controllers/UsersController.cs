24a1a822b401777f3b74bcd917c8639b7e99fb66
Kaloyan Peychev <kokoeverest@gmail.com>
2024-05-28 16:53:45 +0300

    Add Users controller

---
+++ Backend/SponsorSphereWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SponsorSphere.Domain.Models;
+
+namespace SponsorSphereWebAPI.Controllers
+{
+
+    [ApiController]
+    [Authorize]
+    [Route("users/")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;            
+        }
+
+        [HttpGet]
+        [Route("role")]
+        public async Task<IActionResult> GetUserRole()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles.Count == 0)
+            {
+                return NotFound("No roles found for this user");
+            }
+
+            return Ok(new { Role = roles.First() });
+        }
+    }
+}
